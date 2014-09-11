624584eb6a4642732ac68df527d1bf24d83fe6f0
chunjunghee <dkddkd0311@naver.com>
2014-09-11 13:46:56 +0900

    class2

---
+++ class2.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(640, 480);
+  //background(red, green, blue);
+  background(255, 0, 0);
+  stroke(255);
+  noFill();
+
+  line(10, 10, 90, 10); // x1, y1, x2, y2
+    rect(20, 20, 80, 50); // x, y, width, height
+    ellipse(50, 50, 50, 50); // centerX, centerY, width, height
+}
